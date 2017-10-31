4a779b6fb82cbc66770f47af09df5813bfd49216
Yangyang.Xu <yangyang.xu@cootek.cn>
2017-10-31 19:14:10 +0800

    add QrCode组件

--- src/component/QrCode.js
+++ src/component/QrCode.js
@@ -8,7 +8,9 @@ class QrCode extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: false
+      active: false,
+      inputValue: '',
+      textareaValue: ''
     };
   }
   static PropTypes = {
@@ -21,7 +23,19 @@ class QrCode extends Component {
     return (
       <div>
         <div className="btn-style" onClick={this.handleBtnClick}>出现图片</div>
-        <img className="Qr-image" src={require('../image/egg.png')} style={{display: this.state.active ? 'block' : 'none'}}/>
+        <div  style={{display: this.state.active ? 'block' : 'none'}} onClick={this.handleImgClick}>
+          <img className="Qr-image" src={require('../image/egg.png')}/>
+        </div>
+        <div>
+          <p>
+            单行输入框：
+            <input className="input-style" type="text" value={this.state.inputValue} onChange={this.handleInputChange}/>
+          </p>
+          <p>
+            多行输入框：
+            <textarea className="input-style" type="text" value={this.state.textareaValue} onChange={this.handleTextareaChange}/>
+          </p>
+        </div>
       </div>
     );
   }
@@ -30,17 +44,40 @@ class QrCode extends Component {
       active: true
     })
   }
+  handleImgClick = (e) => {
+    //防止事件冒泡
+    e.nativeEvent.stopImmediatePropagation();
+    console.log('stopPropagation');
+  }
+  handleInputChange = (e) => {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  handleTextareaChange = (e) => {
+    this.setState({
+      textareaValue: e.target.value
+    })
+  }
+  //生命周期函数
   //在render方法之前执行
   componentWillMount() {
 
   }
   //在render方法之后执行
   componentDidMount() {
-
+    document.addEventListener('click', e => {
+      if (e.target && e.target.matches('div.btn-style')) {
+        return;
+      }
+      this.setState({
+        active: false
+      })
+    });
   }
   //组件卸载前，常在其中执行一些清理方法，如事件回收或清除定时器
   componentWillUnmount() {
-
+    document.removeEventListener('click');
   }
   componentWillReceiveProps(nextProps) {
 
